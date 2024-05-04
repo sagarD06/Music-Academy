6b549e66b8e78258e2796f4c124573382d9912d8
Sagar D <83652234+sagarD06@users.noreply.github.com>
2024-05-04 18:25:33 +0530

    Final push of the project.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,21 @@
+import FeaturedCourses from "@/components/FeaturedCourses";
+import Footer from "@/components/Footer";
 import HeroSection from "@/components/HeroSection";
+import Instructors from "@/components/Instructors";
+import TestimonialCard from "@/components/TestimonialCard";
+import UpcomingWebinars from "@/components/UpcomingWebinars";
+import WhyChooseUs from "@/components/WhyChooseUs";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen bg-black/[0.96] antialiased flex-col items-center justify-between p-24 bg-grid-white/[0.02]">
-     <HeroSection />
+    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
+      <HeroSection />
+      <FeaturedCourses />
+      <WhyChooseUs />
+      <TestimonialCard />
+      <UpcomingWebinars />
+      <Instructors />
+      <Footer />
     </main>
   );
 }
--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,18 +1,47 @@
+"use client";
 import Link from "next/link";
-import { Spotlight } from "./ui//Spotlight";
+import { TypewriterEffectSmooth } from "./ui/typewriter-effect";
+import { Spotlight } from "./ui/Spotlight";
 import { Button } from "./ui/moving-border";
+
+const words = [
+  {
+    text: "Master",
+    className:
+      "text-center mt-20 md:mt-0 text-4xl md:text-7xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400",
+  },
+  {
+    text: "the",
+    className:
+      "text-center mt-20 md:mt-0 text-4xl md:text-7xl font-bold text-transparent bg-clip-text bg-gradient-to-b from-neutral-50 to-neutral-400",
+  },
+  {
+    text: "art",
+    className:
+      "text-center mt-20 md:mt-0 text-4xl md:text-7xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400",
+  },
+  {
+    text: "of",
+    className:
+      "text-center mt-20 md:mt-0 text-4xl md:text-7xl font-bold text-transparent bg-clip-text bg-gradient-to-b from-neutral-50 to-neutral-400",
+  },
+  {
+    text: "Music.",
+    className:
+      "text-center mt-20 md:mt-0 text-4xl md:text-7xl font-bold text-amber-500 dark:text-amber-500",
+  },
+];
 
 function HeroSection() {
   return (
     <div className="h-auto md:h-[40rem] w-full rounded-md flex flex-col items-center justify-center relative overflow-hidden mx-auto py-10 md:py-0">
-      <Spotlight
-        className="-top-40 left-0 md:left-60 md:-top-20"
-        fill="white"
-      />
+      <Spotlight className="-top-40 left-0 md:left-60 md:-top-20" fill="gold" />
       <div className="relative text-center p-4 z-10 w-full">
-        <h1 className="mt-20 md:mt-0 text-4xl md:text-7xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400">
-          Master the art of Music
-        </h1>
+        <TypewriterEffectSmooth
+          className="flex items-center justify-center"
+          words={words}
+        />
+
         <p className="mt-4 font-normal text-base md:text-lg text-neutral-300 max-w-lg mx-auto">
           Dive into our comprehensive music courses and transform your musical
           journey today. Whether you're a beginner or looking to refine skills,
